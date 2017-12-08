1aeab6c8d9add8c67420037e230727392d6fdae8
Melanie Downing <sayanything830@gmail.com>
2017-12-08 11:28:49 -0800

    working on CRUD testing

--- hector-melanie/server.js
+++ hector-melanie/server.js
@@ -26,7 +26,7 @@ app.get('/new', (request, response) => {
 
 // REVIEW: These are routes for making API calls to enact CRUD operations on our database.
 app.get('/articles', (request, response) => {
-  client.query(`SELECT * FROM articles INNER JOIN authors ON authors.author_id=article.author_id`)
+  client.query(`SELECT * FROM articles INNER JOIN authors ON authors.author_id=articles.author_id;`)
     .then(result => {
       response.send(result.rows);
     })
@@ -39,12 +39,8 @@ app.post('/articles', (request, response) => {
   client.query(
     `INSERT INTO authors(author, "authorUrl")
     VALUES ($1, $2)
-    ON CONFLICT DO NOTHING;
-    ` ,
-    [request.body.author, request.body.authorUrl])
-    .then(() => {
-      response.send('insert complete')
-    });
+    ON CONFLICT DO NOTHING;`,
+    [request.body.author, request.body.authorUrl],
     function(err) {
       if (err) console.error(err);
       // REVIEW: This is our second query, to be executed when this first query is complete.
@@ -54,7 +50,7 @@ app.post('/articles', (request, response) => {
 
   function queryTwo() {
     client.query(
-      `SELECT author_id FROM authors WHERE author=$1`,
+      `SELECT author_id FROM authors WHERE author=$1;`,
       [request.body.author],
       function(err, result) {
         if (err) console.error(err);
@@ -67,12 +63,10 @@ app.post('/articles', (request, response) => {
 
   function queryThree(author_id) {
     client.query(
-      `INSERT INTO articles(author_id, title, author, "authorUrl", category, "publishedOn", body)`,
+      `INSERT INTO articles(author_id, title, category, "publishedOn", body) VALUES($1, $2, $3, $4, $5);`,
       [
         author_id,
         request.body.title,
-        request.body.author,
-        request.body.authorUrl,
         request.body.category,
         request.body.publishedOn,
         request.body.body
@@ -87,13 +81,20 @@ app.post('/articles', (request, response) => {
 
 app.put('/articles/:id', function(request, response) {
   client.query(
-    ``,
-    []
+    `UPDATE authors SET author=$1, "authorUrl"=$2 WHERE author_id=$3;`,
+    [request.body.author, request.body.authorUrl, request.body.author_id]
   )
     .then(() => {
       client.query(
-        ``,
-        []
+        `UPDATE articles SET title=$1, category=$2, "publishedOn"=$3, body=$4 author_id=$5 WHERE article_id=$6;`,
+        [
+          request.body.title,
+          request.body.category,
+          request.body.publishedOn,
+          request.body.body,
+          request.body.author_id,
+          request.params.id
+        ]
       )
     })
     .then(() => {
